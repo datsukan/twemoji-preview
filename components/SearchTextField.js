db86227e8f920733e23f22f459ec80f711022ae9
datsukan <s.datsukan@gmail.com>
2022-04-02 18:59:25 +0900

    :sparkles: update ヘッダーを改善

--- components/SearchTextField.js
+++ components/SearchTextField.js
@@ -2,7 +2,7 @@ import { SearchIcon } from "@heroicons/react/solid"
 import { useSelector, useDispatch } from "react-redux"
 import { set as setWords } from "@store/search"
 
-export const SearchTextField = () => {
+export const SearchTextField = ({ className = "" }) => {
   const words = useSelector(state => state.words)
   const dispatch = useDispatch()
   const changeWords = e => {
@@ -10,7 +10,9 @@ export const SearchTextField = () => {
   }
 
   return (
-    <div className="h-12 mx-4 w-full md:w-10/12 lg:w-2/3 xl:w-1/3 rounded-3xl px-5 border focus-within:shadow-lg hover:shadow-lg bg-white flex items-center gap-2 group">
+    <div
+      className={`rounded-3xl px-5 border focus-within:shadow-lg hover:shadow-lg bg-white flex items-center gap-2 group ${className}`}
+    >
       <SearchIcon className="h-5 w-5 text-gray-400 group-focus-within:text-gray-600" />
       <input
         type="text"
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,19 +1,41 @@
+import { SiteLogo } from "@components/SiteLogo"
 import { SearchTextField } from "@components/SearchTextField"
+import { useEffect, useState } from "react"
 
 export const Header = () => {
-  return (
-    <div className="border-b border-gray-200 px-3">
-      <div className="h-16 max-w-screen-xl mx-auto flex items-center gap-3">
-        <img src="/icon.svg" alt="site icon" className="h-6 w-6 mb-1" />
-        <h1 className="mb-2 text-2xl font-bold text-primary">
-          Twemoji Preview
-        </h1>
-      </div>
+  const [visible, setVisible] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", _ => watchCurrentPosition(), true)
+  }, [])
 
-      {/* 検索 */}
-      <div className="h-40 flex justify-center items-center">
-        <SearchTextField />
+  const watchCurrentPosition = () => {
+    setVisible(scrollTop() > 160)
+  }
+
+  const scrollTop = () => {
+    return Math.max(
+      window.pageYOffset,
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    )
+  }
+
+  return (
+    <>
+      <div className="px-3 bg-gray-900 sticky top-0 z-10">
+        <div className="h-16 max-w-screen-xl mx-auto flex items-center justify-between">
+          <SiteLogo />
+          {visible && (
+            <SearchTextField className="h-9 w-1/2 lg:w-1/3 hidden sm:flex" />
+          )}
+        </div>
       </div>
-    </div>
+      <div className="border-b border-gray-200 px-3">
+        {/* 検索 */}
+        <div className="h-40 flex justify-center items-center">
+          <SearchTextField className="h-12 mx-4 w-full md:w-10/12 lg:w-2/3 xl:w-1/3" />
+        </div>
+      </div>
+    </>
   )
 }
