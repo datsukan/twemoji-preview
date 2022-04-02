7197136e6d296966d438f4fd901cc6b2d32da404
datsukan <s.datsukan@gmail.com>
2022-04-03 00:14:33 +0900

    :sparkles: add SEOおよび外部共有用の設定を追加

---
+++ components/Head.js
@@ -0,0 +1,40 @@
+import NextHead from "next/head"
+
+export const Head = () => {
+  const title = "Twemoji Preview"
+  const description = "Twemoji search and preview site."
+  const image = "/link-card.png"
+  const type = "website"
+
+  return (
+    <NextHead>
+      <title key="title">{title}</title>
+      <meta property="description" content={description} key="description" />
+      <meta property="og:title" content={title} key="og-title" />
+      <meta
+        property="og:description"
+        content={description}
+        key="og-description"
+      />
+      <meta property="og:image" content={image} key="og-image" />
+      <meta property="og:type" content={type} key="og-type" />
+      <meta
+        property="twitter:card"
+        content="summary_large_image"
+        key="twitter-card"
+      />
+      <meta property="twitter:title" content={title} key="twitter-title" />
+      <meta
+        property="twitter:description"
+        content={description}
+        key="twitter-description"
+      />
+      <meta
+        property="twitter:site"
+        content="@datsukan_tech"
+        key="twitter-site"
+      />
+      <meta property="twitter:image" content={image} key="twitter-image" />
+    </NextHead>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import "@styles/globals.css"
+import { Head } from "@components/Head"
 import { Provider } from "react-redux"
 import store from "@store/search"
 import { Toaster } from "react-hot-toast"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Toaster />
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head />
+      <Provider store={store}>
+        <Toaster />
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
