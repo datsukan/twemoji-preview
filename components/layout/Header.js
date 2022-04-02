93478bd818db54593a4aa561211d93d3fed5f790
datsukan <s.datsukan@gmail.com>
2022-04-02 15:56:20 +0900

    :art: add サイトのアイコンとfaviconを設定

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,9 +3,10 @@ import { SearchTextField } from "@components/SearchTextField"
 export const Header = () => {
   return (
     <div className="border-b border-gray-200 px-3">
-      <div className="h-16 max-w-screen-xl mx-auto flex items-center">
-        <h1 className="pb-2 text-2xl font-bold text-primary">
-          🐣 Twemoji Preview
+      <div className="h-16 max-w-screen-xl mx-auto flex items-center gap-3">
+        <img src="/icon.svg" alt="site icon" className="h-6 w-6 mb-1" />
+        <h1 className="mb-2 text-2xl font-bold text-primary">
+          Twemoji Preview
         </h1>
       </div>
 
