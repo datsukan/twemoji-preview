afaae55eeeec544e244f973b5f7f3c97e7e45847
datsukan <s.datsukan@gmail.com>
2022-04-03 13:18:08 +0900

    :art: fix デザインを微修正

--- components/SiteLogo.js
+++ components/SiteLogo.js
@@ -1,8 +1,8 @@
 export const SiteLogo = () => {
   return (
     <div className="h-full flex items-center gap-3">
-      <img src="/icon.svg" alt="site icon" className="h-6 w-6 mb-1" />
-      <h1 className="mb-2 text-2xl font-bold text-white">Twemoji Preview</h1>
+      <img src="/icon.svg" alt="site icon" className="h-6 w-6" />
+      <h1 className="text-2xl font-bold text-white">Twemoji Preview</h1>
     </div>
   )
 }
